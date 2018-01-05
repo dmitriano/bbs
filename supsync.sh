62ea2706e91b2172a55d7b381dd8cc2d7368c619
dmitriano <winappdev@gmail.com>
2018-01-05 13:43:38 +0000

    corrected upgrade script

--- supsync.sh
+++ supsync.sh
@@ -1,4 +1,5 @@
 #aptitude update > update-output.txt
 #(yes '' | aptitude safe-upgrade > upgrade-output.txt)
 apt-get update
-apt-get --with-new-pkgs upgrade
+#apt-get --with-new-pkgs upgrade
+apt-get dist-upgrade
