b405652ca9f6c2baa29741e4c1bc600be6b9dee2
Dmitriano <winappdev@gmail.com>
2018-07-03 03:17:46 +0300

    corrected rig config

--- nvidia/rig-config.sh
+++ nvidia/rig-config.sh
@@ -1,4 +1,5 @@
 export DISPLAY=:0
 nvidia-settings -a [gpu:0]/GPUPowerMizerMode=1
-nvidia-settings -a [gpu:0]/GPUFanControlState=1;
-nvidia-settings -a [fan:0]/GPUTargetFanSpeed=90;
+nvidia-settings -a [gpu:0]/GPUFanControlState=1
+nvidia-settings -a [fan:0]/GPUTargetFanSpeed=90
+
